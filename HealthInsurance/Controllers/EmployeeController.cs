64e7e386957116b722e3cdd0674df3abfa26edee
nhatanhn36 <nhatanhn36@gmail.com>
2023-03-24 17:00:27 +0700

    Add project files.

---
+++ HealthInsurance/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HealthInsurance.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthInsurance.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly HealthInsuranceContext _context;
+
+        public EmployeeController(HealthInsuranceContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HealthInsurance/Controllers/RegisterController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HealthInsurance.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace HealthInsurance.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly HealthInsuranceContext _context;
+
+        public RegisterController(HealthInsuranceContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(UserLogin register)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new UserLogin
+                {
+                    Username = register.Username,
+                    Password = register.Password
+                };
+
+                _context.UserLogins.Add(user);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Login", "Login");
+            }
+
+            return View(register);
+        }
+    }
+}
